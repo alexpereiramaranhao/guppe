cf15035a1052ec2d088d5256446bc88f0ccbd168
ALEX PEREIRA MARANHAO <alexmaranhao@saneago.com.br>
2020-02-03 13:38:44 -0300

    Adiciona funções com strings

---
+++ tipo_string.py
@@ -0,0 +1,25 @@
+# aspas duplas
+nome = "Alex"
+print(nome)
+
+# aspas simples
+nome = 'Alex'
+print(nome)
+
+# aspas triplas duplas
+nome = '''Alex\nPereira Maranhão'''
+print(nome.lower())
+
+# aspas triplas simples
+nome = """Alex's bar"""
+print(nome.upper())
+
+# split
+nome = """Alex Pereira Maranhão"""
+print(nome.split())
+print(nome[0:4])
+print(nome[5:nome.__len__()])
+
+# começa do primeiro elemento, vá até o último e inverte
+print(nome[::-1])
+print()
