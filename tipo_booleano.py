45eba6df3977330f33004ffc67d9766b7a59f05a
ALEX PEREIRA MARANHAO <alexmaranhao@saneago.com.br>
2020-02-03 13:23:17 -0300

    Documenta tipo boolean

---
+++ tipo_booleano.py
@@ -0,0 +1,25 @@
+"""
+True - verdadeiro
+False - false
+
+Errado:
+true
+false
+
+"""
+logado = True
+ativo = False
+
+print(ativo)
+
+# Negação
+
+print(not ativo)
+
+# or
+
+print(logado or ativo)
+
+# and
+print(logado and ativo)
+
