de712200443a1b89a8f898b073625a984d7431c7
Alex Pereira Maranhão <alexpereira365@gmail.com>
2020-02-21 16:09:50 -0300

    Finaliza teoria dos conjuntos em python

--- conjuntos.py
+++ conjuntos.py
@@ -20,3 +20,48 @@ conjunto = set({1, 2, 3, 4, 5, 6, 7, 7})
 
 print(type(conjunto))
 print(conjunto)
+
+# forma mais comum de declarar
+
+conjunto = {1, 2, 3, 3,  3, 4, 7, 9}
+
+print(type(conjunto))
+print(conjunto)
+
+if 3 in conjunto:
+    print("número encontrado.")
+
+conjunto.add(10)
+print(conjunto)
+
+conjunto.remove(10)  # caso não seja encontrado, gera excecão KeyError
+conjunto.discard(10)  # caso não seja encontrado, não gera erro
+print(conjunto)
+
+lista = ["Goiânia", "São Paulo", "Cuiabá", "Anápolis", "Goiânia", "Goiânia"]
+print(f"Quantidade de usuários {len(lista)}")
+print(f"Cidades participantes {set(lista)}")
+
+print("-----------------------------------------------------")
+
+# Métodos matemáticos dos conjuntos
+
+pythonSet = {"Marcos", "Alex", "Júlia", "Antônio"}
+javaSet = {"Alex", "Fernando", "Júlia", "Guthierrez", "Diogo"}
+
+# Gerar um conjunto de nomes únicos
+alunosUnicos = pythonSet.union(javaSet)
+print(alunosUnicos)
+alunosUnicos = pythonSet | javaSet
+print(alunosUnicos)
+
+# só estuda em um
+alunosUnicos3 = pythonSet.difference(javaSet)
+print(alunosUnicos3)
+
+# estuda em ambos
+alunosUnicos4 = pythonSet.intersection(javaSet)
+print(alunosUnicos4)
+alunosUnicos4 = pythonSet & javaSet
+print(alunosUnicos4)
+
